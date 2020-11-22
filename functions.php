f9a9faa126eb1b6ce7208ad4db5cfaee05f3c8d2
Nicolas Métivier <nicolasmetivier@MacBook-Pro-de-Nicolas.home>
2020-11-22 19:23:18 +0100

    first commit

---
+++ functions.php
@@ -0,0 +1,15 @@
+<?php
+function wpm_enqueue_styles(){
+wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+add_action( 'wp_enqueue_scripts', 'wpm_enqueue_styles' );
+
+// OVERIDE HEADER PHP 
+add_action('wp_head', 'wpse_43672_wp_head');
+function wpse_43672_wp_head(){
+    ?>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <?php
+}
---
+++ templates/template-home.php
@@ -0,0 +1,12 @@
+<?php 
+/* Template name: Home */ 
+get_header(); 
+?>
+<main>
+    <section>
+        <div>
+    
+        </div>
+    </section>
+</main>
+<?php get_footer(); ?>
